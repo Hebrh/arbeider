9bfdb02b9c8353a8e76720d4683292757e3f0bbf
lipicoder <lipicoder@qq.com>
2023-01-24 18:19:21 +0800

    test versioneer

--- src/arbeider/__init__.py
+++ src/arbeider/__init__.py
@@ -1,9 +1,11 @@
-def start():
+from . import _version
+
+__version__ = _version.get_versions()['version']
+
+
+def run():
     """Start the arbeider daemon."""
     print("Starting arbeider worker...")
 
 
-__all__ = ["start"]
-
-from . import _version
-__version__ = _version.get_versions()['version']
+__all__ = ["run"]
